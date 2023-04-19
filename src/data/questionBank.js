2e828e2f9be09946f95e4697c7f649097a07097c
Anjali Ghanshani <anjali.ghanshani@gmail.com>
2023-04-19 23:16:28 +1000

    Reformatted question box
    - input and output format are now separate from question
    - respective input and output values passed to question box

--- src/data/questionBank.js
+++ src/data/questionBank.js
@@ -1,8 +1,9 @@
 export const questionBank = [
   {
     id: 1,
-    question:
-      'Write a program that takes a number N as the input, and prints it to the output. \n\n Input Format: The only line contains a single integer.\n\n Output Format: Output the answer in a single line.',
+    question: 'Write a program that takes a number N as the input, and prints it to the output.',
+    input_format: 'The only line contains a single integer.',
+    output_format: 'Output the answer in a single line.',
     testcase: [
       {
         input: '123',
@@ -35,6 +36,8 @@ export const questionBank = [
     id: 2,
     question:
       'Chef and Chefina are playing with dice. In one turn, both of them roll their dice at once. They consider a turn to be good if the sum of the numbers on their dice is greater than Given that in a particular turn Chef and Chefina got X and Y on their respective dice, find whether the turn was good.',
+    input_format: 'Container two integers, second one is the one that gets computed',
+    output_format: 'Prints out YES or NO string',
     testcase: [
       {
         input: '1 4',
@@ -65,8 +68,10 @@ export const questionBank = [
   },
   {
     id: 3,
-    question:
-      'Given two integers A and B, write a program to add these two numbers and output it. \n\n Input Format: The first line contains an integer T, the total number of test cases. Then follow T lines, each line contains two Integers A and B. \n\n Output Format: For each test case, add A and B and display the sum in a new line.',
+    question: 'Given two integers A and B, write a program to add these two numbers and output it.',
+    input_format:
+      'The first line contains an integer T, the total number of test cases. Then follow T lines, each line contains two Integers A and B.',
+    output_format: 'For each test case, add A and B and display the sum in a new line.',
     testcase: [
       {
         input: '1 2',
@@ -98,7 +103,11 @@ export const questionBank = [
   {
     id: 4,
     question:
-      "Chef wants to become fit for which he decided to walk to the office and return home by walking. It is known that Chef's office is X km away from his home. If his office is open on days in a week, find the number of kilometers Chef travels through office trips in a week. \n\n Input Format: First line will contain \n\n T, number of test cases. Then the test cases follow. Each test case contains of a single line consisting of single integer X. \n\n Output Format: For each test case, output the number of kilometers Chef travels through office trips in a week.",
+      "Chef wants to become fit for which he decided to walk to the office and return home by walking. It is known that Chef's office is X km away from his home. If his office is open on days in a week, find the number of kilometers Chef travels through office trips in a week.",
+    input_format:
+      'First line will contain T, number of test cases. Then the test cases follow. Each test case contains of a single line consisting of single integer X.',
+    output_format:
+      'For each test case, output the number of kilometers Chef travels through office trips in a week.',
     testcase: [
       {
         input: '1',
@@ -130,7 +139,11 @@ export const questionBank = [
   {
     id: 5,
     question:
-      'Chef is playing Ludo. According to the rules of Ludo, a player can enter a new token into the play only when he rolls a on the die. In the current turn, Chef rolled the number X on the die. Determine if Chef can enter a new token into the play in the current turn or not. \n\n Input Format: The first line contains a single integer T — the number of test cases. Then the test cases follow. The first and only line of each test case contains one integer. X — the number rolled by the Chef on the die. \n\n Output Format: For each test case, output YES if the Chef can enter a new token in the game. Otherwise, output NO. You may print each character of YES and NO in uppercase or lowercase (for example, yes, yEs, Yes will be considered identical).',
+      'Chef is playing Ludo. According to the rules of Ludo, a player can enter a new token into the play only when he rolls a on the die. In the current turn, Chef rolled the number X on the die. Determine if Chef can enter a new token into the play in the current turn or not.',
+    input_format:
+      'The first line contains a single integer T — the number of test cases. Then the test cases follow. The first and only line of each test case contains one integer. X — the number rolled by the Chef on the die.',
+    output_format:
+      'For each test case, output YES if the Chef can enter a new token in the game. Otherwise, output NO. You may print each character of YES and NO in uppercase or lowercase (for example, yes, yEs, Yes will be considered identical).',
     testcase: [
       {
         input: '1',
@@ -162,13 +175,3 @@ export const questionBank = [
 ]
 
 export default questionBank
-// Write function that reverses a list, preferably in place.
-// Write a function that checks whether an element occurs in a list.
-// Write a function that returns the elements on odd positions in a list.
-// Write a function that computes the running total of a list.
-// Write a function that tests whether a string is a palindrome.
-// Write a function that concatenates two lists. [a, b, c], [1, 2, 3] →[a, b, c, 1, 2, 3].
-// Write a function that takes a number and returns a list of its digits.So for 2342 it should return [2, 3, 4, 2].
-
-// Write a program in C that takes minutes as input, and display the total number of hours and minutes.
-// Write a program that converts Centigrade to Fahrenheit.
